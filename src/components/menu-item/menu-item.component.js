acce29d1057ad76bca472182971d4e9316c6f32c
baban666 <baban666@tut.by>
2020-01-24 16:34:45 +0300

    update router
    HOC router

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,10 +1,13 @@
 import React from "react";
 import './menu-item.styles.scss';
+import {withRouter} from 'react-router-dom';
 
 
-const MenuItem = ({title, imageUrl, size}) => (
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
     <div
-        className={`${(size) ? size : ''} menu-item`}>
+        className={`${(size) ? size : ''} menu-item`}
+        onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
         <div
             style={{backgroundImage: `url(${imageUrl})`}}
             className='background-image'
@@ -15,4 +18,4 @@ const MenuItem = ({title, imageUrl, size}) => (
         </div>
     </div>
 );
-export default MenuItem
+export default withRouter(MenuItem);
